e0ff97ef7cae8e54e4df76633335d9eea0e32067
rajiv.sambasivan@gmail.com <rajiv.sambasivan@arangodb.com>
2021-08-05 18:47:13 +0530

    Adding skeleton code for implementation.

---
+++ src/data_processing/retail_data_processing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 18:38:15 2021
+
+@author: Rajiv Sambasivan
+"""
+
+from abstract_data_processing import DataProcessing
+
+class RetailDataProcessing(DataProcessing):
+    
+    def filter(self, **kwargs):
+        pass
+    
+    def transform(self, **kwargs):
+        pass
+    
+    
+    def subset(self, **kwargs):
+        pass

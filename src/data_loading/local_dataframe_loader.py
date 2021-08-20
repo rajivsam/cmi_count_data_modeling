58da31e3bc1b2a87eb5f89ec70e67d64cdc0d0e3
Sama-Ko <samanwitakoley@gmail.com>
2021-08-20 21:34:10 +0530

    Constructed class for local data loader

--- src/data_loading/local_dataframe_loader.py
+++ src/data_loading/local_dataframe_loader.py
@@ -6,9 +6,11 @@ Created on Thu Aug  5 18:11:57 2021
 @author: Rajiv Sambasivan
 """
 from abstract_dataframe_loader import DataFrameLoader
+import pandas as pd
 
 class LocalDataFrameLoader(DataFrameLoader):
     
     def get_dataframe(self):
-        pass
+        self.df= pd.read_excel('https://github.com/rajivsam/cmi_count_data_modeling/blob/data_branch/data/Online%20Retail.xlsx?raw=true')
+        return self.df
  

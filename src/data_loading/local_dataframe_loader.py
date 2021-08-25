3c5b5f05b052fc609ee8da4020c4a62bce7dddf6
Sama-Ko <samanwitakoley@gmail.com>
2021-08-25 19:36:36 +0530

    Added docstring

--- src/data_loading/local_dataframe_loader.py
+++ src/data_loading/local_dataframe_loader.py
@@ -9,8 +9,14 @@ from abstract_dataframe_loader import DataFrameLoader
 import pandas as pd
 
 class LocalDataFrameLoader(DataFrameLoader):
+    """ Creates the dataframe on which the project is to be done."""
     
     def get_dataframe(self):
+        """ Loads the dataset of the project and converts it into a pandas dataframe
+        
+        Input of this method is the dataset of the project. This method converts the dataset into a pandas dataframe and returns the dataframe.
+        """
+        
         self.df= pd.read_excel('https://github.com/rajivsam/cmi_count_data_modeling/blob/data_branch/data/Online%20Retail.xlsx?raw=true')
         return self.df
  
